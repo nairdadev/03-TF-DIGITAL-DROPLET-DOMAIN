2ebbc1410772d133d3473089eac0f10baf204c62
Adrián <42869274+adriancanog@users.noreply.github.com>
2021-05-30 01:45:49 -0300

    added commets

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
+#Se indica una variable para el token de Digital Ocean
 variable token_do {
 type = string
 description = "Token de Digital Ocean"
 }
 
-#Pass provider
-#export TF_VAR_token_do=""
+#Provider 
 provider "digitalocean" {
   token = var.token_do
 }
 
 
-
+#Traemos nuestra SHH-KEY
 resource "digitalocean_ssh_key" "web" {
   name       = "sshkeys"
   public_key = file("${path.module}/files/id_rsa.pub")
 }
 
-#Droplet
+#Droplet Creación
 resource "digitalocean_droplet" "web" {
   image              = "ubuntu-20-04-x64"
   name               = "web"
@@ -24,10 +24,11 @@ resource "digitalocean_droplet" "web" {
   size               = "s-1vcpu-1gb"
   monitoring         = true
   private_networking = true
+  #Creación de SSH KEY EN DO
   ssh_keys = [
     digitalocean_ssh_key.web.id
   ]
-
+  #SH para configurar proyecto en ubuntu
   user_data = file("${path.module}/files/user-data.sh")
 }
 
